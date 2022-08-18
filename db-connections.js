d1d08347a752b1e7beeda51a1e3d6a5bbc65a55d
belugaga <109129621+belugaga@users.noreply.github.com>
2022-08-18 23:27:13 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 var connection  = mysql.createConnection({
-    host:'localhost',
+    host:'database-1.cjb5vrcyxql7.ap-southeast-1.rds.amazonaws.com',
     port:'3306',
-    user:'root',
-    password:'cdev',
+    user:'admin',
+    password:'tG078568',
     database:'book_review'
 });
 
 connection.connect(err => {
     if(err) throw err;
-    console.log('Connected to DB');
+    console.log('Connected to AWS DB');
 });
 
 module.exports = connection;
